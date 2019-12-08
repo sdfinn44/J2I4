1fd2516867f77c02aff031d8c3e7479a22ce490d
Stephen Finn <steve.d.finn@gmail.com>
2019-12-08 15:45:11 +1100

    Added UI

---
+++ J2I4/TestData/Clients.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using IdentityServer4;
+using IdentityServer4.Models;
+
+namespace J2I4.TestData
+{
+    internal class Clients
+    {
+        public static IEnumerable<Client> Get()
+        {
+            return new List<Client>
+            {
+                new Client
+                {
+                    ClientId = "oauthClient",
+                    ClientName = "Example Client Credentials Client Application",
+                    AllowedGrantTypes = GrantTypes.ClientCredentials,
+                    ClientSecrets = new List<Secret>
+                    {
+                        new Secret("superSecretPassword".Sha256())
+                    },
+                    AllowedScopes = new List<string> {"customAPI.read"}
+                },
+                new Client {
+                    ClientId = "openIdConnectClient",
+                    ClientName = "Example Implicit Client Application",
+                    AllowedGrantTypes = GrantTypes.Implicit,
+                    AllowedScopes = new List<string>
+                    {
+                        IdentityServerConstants.StandardScopes.OpenId,
+                        IdentityServerConstants.StandardScopes.Profile,
+                        IdentityServerConstants.StandardScopes.Email,
+                        "role",
+                        "customAPI.write"
+                    },
+                    RedirectUris = new List<string> {"http://localhost:52467/signin-oidc"},
+                    PostLogoutRedirectUris = new List<string> {"http://localhost:52467"}
+                }
+            };
+        }
+    }
+}
